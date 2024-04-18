0081d0dc4bded25282fe178b742c327e6d653a7d
Milos Marinkovic <marinkovicmilos77@gmail.com>
2024-04-18 12:31:07 +0200

    add and remove recipe

--- src/controllers/recipesController.mjs
+++ src/controllers/recipesController.mjs
@@ -11,7 +11,7 @@ export const getRecipes = async (request, reply) => {
 
 export const getRecipeById = async (request, reply) => {
     try {
-        const recipeId = +request.params.id;
+        const recipeId = request.params.id;
         const recipe = await Recipe.findById(recipeId);
 
         if (!recipe) {
@@ -20,6 +20,37 @@ export const getRecipeById = async (request, reply) => {
 
         return reply.send(recipe);
     } catch (error) {
-        reply.status(500).send({ error: 'Internal Server Error' });
+        reply.status(500).send({ error: error.message });
+    }
+};
+
+export const addRecipe = async (request, reply) => {
+    try {
+        const { name, ingredients, steps } = request.body;
+
+        const recipe = new Recipe({ name, ingredients, steps });
+
+        await recipe.save();
+
+        return recipe;
+    } catch (error) {
+        reply.status(500).send({ error: error.message });
+    }
+};
+
+export const deleteRecipe = async (request, reply) => {
+    try {
+        const recipeId = request.params.id;
+
+        const deletedRecipe = await Recipe.findByIdAndDelete(recipeId);
+
+        if (!deletedRecipe) {
+            reply.status(404).send({ error: 'Recipe not found' });
+            return;
+        }
+
+        return { message: `Recipe with ID ${recipeId} deleted successfully` };
+    } catch (error) {
+        reply.status(500).send({ error: error.message });
     }
 };
--- src/routes/recipesRoutes.mjs
+++ src/routes/recipesRoutes.mjs
@@ -1,8 +1,10 @@
-import { getRecipes, getRecipeById } from '../controllers/recipesController.mjs';
+import { getRecipes, getRecipeById, addRecipe, deleteRecipe } from '../controllers/recipesController.mjs';
 
 export async function routes(fastify, options) {
     fastify.get('/recipes', getRecipes);
     fastify.get('/recipes/:id', getRecipeById);
+    fastify.post('/recipes', addRecipe);
+    fastify.delete('/recipes/:id', deleteRecipe);
 }
 
 export default routes;
