e33ccf15727cf0f51eb6057ab84f9b8d22956d41
marinkovicmilos <39673946+marinkovicmilos@users.noreply.github.com>
2024-04-18 11:58:31 +0200

    added mongodb (#2)

--- src/models/recipeModel.mjs
+++ src/models/recipeModel.mjs
@@ -1,13 +1,9 @@
-const recipes = [
-    { id: 1, name: 'Pasta Carbonara', ingredients: ['pasta', 'eggs', 'bacon', 'cheese'], steps: ['Cook pasta', 'Fry bacon', 'Mix eggs and cheese', 'Combine everything'] },
-    { id: 2, name: 'Chicken Curry', ingredients: ['chicken', 'curry sauce', 'rice'], steps: ['Cook chicken', 'Prepare curry sauce', 'Serve with rice'] }
-];
+import mongoose from 'mongoose';
 
-export const Recipe = {
-    async find() {
-        return recipes;
-    },
-    async findById(id) {
-        return recipes.find(recipe => recipe.id === id);
-    }
-};
+const recipeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    ingredients: [{ type: String, required: true }],
+    steps: [{ type: String, required: true }]
+});
+
+export const Recipe = mongoose.model('Recipe', recipeSchema);
--- src/server.mjs
+++ src/server.mjs
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import recipesRoutes from './routes/recipesRoutes.mjs';
+import mongoose from 'mongoose';
 
 const port = 3000;
 const fastify = Fastify({
@@ -8,8 +9,16 @@ const fastify = Fastify({
 
 fastify.register(recipesRoutes);
 
+const connectToDatabase = async () => {
+    const uri = 'mongodb://localhost:27017/cookbook';
+    mongoose.connect(uri)
+        .then(() => console.log('Connected to MongoDB'))
+        .catch(err => console.error('Error connecting to MongoDB:', err));
+};
+
 const start = async () => {
     try {
+        connectToDatabase();
         fastify.listen({ port }, (err, address) => {
             if (err) throw err
             console.log(`Server listening on ${address}`);
