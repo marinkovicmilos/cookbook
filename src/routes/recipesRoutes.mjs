d4f0e9e9744b39fe841d6b508d0d06af6ab1a23f
marinkovicmilos <39673946+marinkovicmilos@users.noreply.github.com>
2024-04-19 10:56:04 +0200

    authentication (#4)

--- src/routes/recipesRoutes.mjs
+++ src/routes/recipesRoutes.mjs
@@ -1,6 +1,6 @@
 import { getRecipes, getRecipeById, addRecipe, updateRecipe, deleteRecipe } from '../controllers/recipesController.mjs';
 
-export async function routes(fastify, options) {
+async function routes(fastify, options) {
     fastify.get('/recipes', getRecipes);
     fastify.get('/recipes/:id', getRecipeById);
     fastify.post('/recipes', addRecipe);
